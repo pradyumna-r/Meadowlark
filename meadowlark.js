af0a5f784f5992fc6d6cc8915165f0b1e4a1606f
pradyumna <pradyumna@DESKTOP-C4FNH71>
2023-04-13 18:23:47 +0530

    moved fortunes array to a module

--- meadowlark.js
+++ meadowlark.js
@@ -1,16 +1,8 @@
 const express=require('express');
 const expressHandlebars=require('express-handlebars');
+const fortune=require('./lib/fortune.js');
 const app = express();
 const port=process.env.port || 3000;
-
-const fortunes=[
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-   ]
-   
 
 app.engine('handlebars', expressHandlebars.engine());
 app.set('view engine','handlebars');
@@ -23,8 +15,7 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/about',(req,res)=>{
-    const randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about',{fortune:randomFortune});
+    res.render('about',{fortune:fortune.getFourtune});
 })
 
 //cusotm 404 pages
