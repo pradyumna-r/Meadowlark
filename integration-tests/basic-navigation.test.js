ccc7e4c8050cfb232ad2f44c89062ad87307c2a2
pradyumna-r <pradyumna@openinapp.com>
2023-04-15 20:49:53 +0530

    pulled into mac,made some changes,running now

--- integration-tests/basic-navigation.test.js
+++ integration-tests/basic-navigation.test.js
@@ -1,30 +1,28 @@
-const portfinder=require('portfinder');
-const puppeteer=require('puppeteer');
+const portfinder = require("portfinder");
+const puppeteer = require("puppeteer");
 
-const app=require('../meadowlark.js');
+const app = require("../meadowlark.js");
 
-let server=null;
-let port=null;
+let server = null;
+let port = null;
 
-beforeEach(async ()=>{
-    port = await portfinder.getPortPromise();
-    server=app.listen(port);
-})
-
-afterEach(()=>{
-    server.close();
-})
-
-test("home page links to about page",async ()=>{
-    const browser=await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-bsu'
-      });
-    const page=await browser.newPage();
-    await page.goto(`http://localhost:${port}`);
-    await promise.all([
-        page.waitforNavigation(),
-        page.click('[data-test-id="about"]'),
-    ])
-    expect(page.url()).toBe(`http://localhost:${port}/about`);
-    await browser.close()
+beforeEach(async () => {
+  port = await portfinder.getPortPromise();
+  server = app.listen(port);
 });
+
+afterEach(() => {
+  server.close();
+});
+
+test("home page links to about page", async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${port}`);
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('[data-test-id="about"]'),
+  ]);
+  expect(page.url()).toBe(`http://localhost:${port}/about`);
+  await browser.close();
+}, 10000);
